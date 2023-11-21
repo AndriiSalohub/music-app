a38a855ef7dabd55ed4cd349b34f03bb236b7b0e
Andrii <andrejsalogub3@gmail.com>
2023-11-21 16:33:36 +0200

    added DOMContentLoaded event to prevent potential bugs

--- js/main.js
+++ js/main.js
@@ -1,66 +1,70 @@
 import tracksList from "../assets/tracksList";
 
-const list = document.querySelector(".main__list");
-const searchInput = document.querySelector(".main__search-input");
+document.addEventListener("DOMContentLoaded", () => {
+    const list = document.querySelector(".main__list");
+    const searchInput = document.querySelector(".main__search-input");
 
-const tracksListWithElem = [...tracksList].map((track) => {
-    const listItem = document.createElement("li");
-    listItem.className = "main__list__item";
-    let duration =
-        Math.floor(track.duration / 60) > 10
-            ? `${Math.floor(track.duration / 60)}:${
-                  track.duration % 60 > 10
-                      ? track.duration % 60
-                      : "0" + (track.duration % 60)
-              }`
-            : `0${Math.floor(track.duration / 60)}:${
-                  track.duration % 60 > 10
-                      ? track.duration % 60
-                      : "0" + (track.duration % 60)
-              }`;
-    listItem.innerHTML = `
-        <button class="main__list__item_play-btn">&#9654;</button>
-        <img class="main__list__item_img" src=${track.preview} alt=${track.title} />
-        <div class="main__list__item__info">
-            <h2 class="main__list__item__info_title">${track.title}</h2>
-            <p class="main__list__item__info_author">${track.artists}</p>
-        </div>
-        <p class="main__list__item_duration">${duration}</p>
-    `;
+    const tracksListWithElem = [...tracksList].map((track) => {
+        const listItem = document.createElement("li");
+        listItem.className = "main__list__item";
+        let duration =
+            Math.floor(track.duration / 60) > 10
+                ? `${Math.floor(track.duration / 60)}:${
+                      track.duration % 60 > 10
+                          ? track.duration % 60
+                          : "0" + (track.duration % 60)
+                  }`
+                : `0${Math.floor(track.duration / 60)}:${
+                      track.duration % 60 > 10
+                          ? track.duration % 60
+                          : "0" + (track.duration % 60)
+                  }`;
+        listItem.innerHTML = `
+            <button class="main__list__item_play-btn">&#9654;</button>
+            <img class="main__list__item_img" src=${track.preview} alt=${track.title} />
+            <div class="main__list__item__info">
+                <h2 class="main__list__item__info_title">${track.title}</h2>
+                <p class="main__list__item__info_author">${track.artists}</p>
+            </div>
+            <p class="main__list__item_duration">${duration}</p>
+        `;
 
-    list.append(listItem);
-    return {
-        ...track,
-        element: listItem,
-        play: false,
-        statusElement: listItem.querySelector(".main__list__item_play-btn"),
-    };
-});
-
-searchInput.addEventListener("input", (e) => {
-    tracksListWithElem.forEach((track) => {
-        track.element.classList.toggle(
-            "hide",
-            !(
-                track.title.toLocaleLowerCase().includes(e.target.value) ||
-                track.artists.toLocaleLowerCase().includes(e.target.value)
-            )
-        );
+        list.append(listItem);
+        return {
+            ...track,
+            element: listItem,
+            play: false,
+            statusElement: listItem.querySelector(".main__list__item_play-btn"),
+        };
     });
-});
 
-const playButtons = document.querySelectorAll(".main__list__item_play-btn");
-
-playButtons.forEach((playButton, index) => {
-    playButton.addEventListener("click", () => {
+    searchInput.addEventListener("input", (e) => {
         tracksListWithElem.forEach((track) => {
-            if (track.id === index + 1) {
-                track.play = !track.play;
-                track.statusElement.innerHTML = track.play ? "⏸" : "&#9654;";
-            } else {
-                track.play = false;
-                track.statusElement.innerHTML = "&#9654;";
-            }
+            track.element.classList.toggle(
+                "hide",
+                !(
+                    track.title.toLocaleLowerCase().includes(e.target.value) ||
+                    track.artists.toLocaleLowerCase().includes(e.target.value)
+                )
+            );
+        });
+    });
+
+    const playButtons = document.querySelectorAll(".main__list__item_play-btn");
+
+    playButtons.forEach((playButton, index) => {
+        playButton.addEventListener("click", () => {
+            tracksListWithElem.forEach((track) => {
+                if (track.id === index + 1) {
+                    track.play = !track.play;
+                    track.statusElement.innerHTML = track.play
+                        ? "⏸"
+                        : "&#9654;";
+                } else {
+                    track.play = false;
+                    track.statusElement.innerHTML = "&#9654;";
+                }
+            });
         });
     });
 });
